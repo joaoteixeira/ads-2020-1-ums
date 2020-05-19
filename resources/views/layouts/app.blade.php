8e20826a60d236865130187ee0485c1fd56aa125
Paulo Rodrigo <paulo.rodrigo@credisis.com.br>
2020-05-19 02:30:53 -0300

    Mudanças relacionadas as aulas dias 04/05, 11/05 e 19/05

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+
+    <title>@yield('title')</title>
+  </head>
+  <body>
+    
+    @yield('conteudo')
+
+    <script src="{{ asset('js/jquery-3.5.1.slim.min.js') }}"></script>
+    <script src="{{ asset('js/popper.min.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
+  </body>
+</html>
---
+++ resources/views/sobre.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.principal')
+@section('title', 'Sobre')
+@section('title-content', 'Título Sobre')
+@section('content')
+    <div class="title m-b-md">
+        Página sobre do sistema, usando blande template
+    </div>
+@endsection
